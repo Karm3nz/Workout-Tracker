086f993a70ffb6604813d7a5e6b0d9813e832fd3
Carmen Hui <karmenzkarmenz@gmail.com>
2021-02-11 14:39:08 -0500

    modified apiRoutes.js
    added limit to the days displayed on the graphs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -35,6 +35,7 @@ router.get("/api/workouts", (req, res) => {
 
 router.get("/api/workouts/range", (req, res) => {
     Workout.find({})
+    .limit(9)
     .then(dbWorkout => {
         res.json(dbWorkout);
     })
